d855ff4ef0e604af90f7ce4748f7c5a4d6f07270
Renato Portela <renato.portela@gmail.com>
2021-04-25 22:09:13 +0000

    Update UI to match backend

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -3,20 +3,36 @@ import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
-      return (  
-        <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
-          <table className="table table-bordered">
-            <tbody>
-            {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
-            }
-            </tbody>
-          </table>
-          </div>
-          );
-        }
+        let ctr = 0;  
+        return (  
+            <div class="row justify-content-center">
+            <div class="col-auto">
+            <table className="table table-responsive table-striped w-auto ">
+                <thead>
+                        <tr >
+                            <th>#</th>
+                            <th>Emotions</th>
+                            <th>Score</th>
+                        </tr>
+                    </thead>
+                <tbody>
+                {
+                    // Use the .map method to extract the emotions
+                    Object.entries(this.props.emotions).map( emotion => (
+                        <tr>
+                            <td>{ctr++}</td>
+                            <td>{emotion[0]}</td>
+                            <td>{emotion[1]}</td>
+                        </tr>
+                    ))
+
+                }
+                </tbody>
+            </table>
+            </div>
+            </div>
+        );
+    }
     
 }
 export default EmotionTable;
